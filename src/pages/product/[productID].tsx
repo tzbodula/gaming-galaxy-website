8a61e322c29c7060cbcfc4494467fe1e0e6424c9
Thomas Zbodula <tzbodula@gmail.com>
2022-06-02 15:02:40 -0400

    Changed schema around

--- src/pages/product/[productID].tsx
+++ src/pages/product/[productID].tsx
@@ -221,7 +221,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
     const productData = await prisma.merch.findUnique({
         where: {
-            type: productType
+            id: productType
         }
     })
 
