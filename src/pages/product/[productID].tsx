511c1e0cd51f898ed913ce6a8b892e7d7a3aa28a
Thomas Zbodula <tzbodula@gmail.com>
2022-06-02 15:26:39 -0400

    Nope not fixed :(

--- src/pages/product/[productID].tsx
+++ src/pages/product/[productID].tsx
@@ -221,7 +221,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
     const productData = await prisma.merch.findUnique({
         where: {
-            id: 'Case'
+            id: productType
         }
     })
 
